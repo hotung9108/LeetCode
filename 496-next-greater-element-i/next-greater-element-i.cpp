f00a031fb9e6305c63247d96b939d966cb07746e
HoTung <139144999+hotung9108@users.noreply.github.com>
2026-01-19 16:31:15 +0700

    Time: 39 ms (5.19%) | Memory: 13.3 MB (13.91%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -1,20 +1,30 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        // map: số -> danh sách index xuất hiện trong nums2
         unordered_map<int, vector<int>> mp;
-        for(int i = 0; i < nums2.size(); i++){
+        for (int i = 0; i < nums2.size(); i++) {
             mp[nums2[i]].push_back(i);
         }
+
         vector<int> res;
-        for(int i = 0; i < nums1.size(); i++){
+        for (int i = 0; i < nums1.size(); i++) {
             int index = mp[nums1[i]][0];
             int nextGreater = -1;
-            for(int j = index + 1; j < nums2.size(); j++){
-                if(nums2[j] > nums1[i]){
-                    nextGreater = nums2[j];
-                    break;
+            for (auto &kv : mp) {
+                int key = kv.first;
+                if (key > nums1[i]) {
+                    for (int pos : kv.second) {
+                        if (pos > index) {
+                            if (nextGreater == -1 || pos < mp[nextGreater][0]) {
+                                nextGreater = key;
+                            }
+                            break; 
+                        }
+                    }
                 }
             }
+
             res.push_back(nextGreater);
         }
         return res;
